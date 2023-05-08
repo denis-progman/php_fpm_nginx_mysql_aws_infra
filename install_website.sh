033d70ac51b0f78dc916831b801ec61efc414f95
denis-pogman <denis.progman@gmail.com>
2023-05-07 23:54:43 -0400

    built vpc, subnets and ec2 with nginx entre point

---
+++ install_website.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+sudo su 
+sudo amazon-linux-extras install -y epel
+yum update -y 
+yum install -y nginx 
+systemctl enable nginx
+systemctl start nginx
+cat > /etc/nginx/conf.d/site.conf<<EOM
+    server {
+        listen       80;
+        listen       [::]:80;
+        server_name  _;
+        root         /var/www/html;
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+        location = /404.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+        }
+    }
+EOM
+
+echo > /var/www/html/index.html "<h1>Hello world!</h1>"
+nginx -s reload
